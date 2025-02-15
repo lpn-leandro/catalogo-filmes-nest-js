5a3f5f50f9276de0e505dc4806af9c992edbb4ef
Leandro <108540053+lpn-leandro@users.noreply.github.com>
2025-02-15 14:03:51 -0300

    Feature: Define modules and services (#1)
    * Defined modules and services
    
    * update on readme.md

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,31 +1,44 @@
-import { Controller, Get, Post, Put, Delete, Param, Body } from '@nestjs/common';
+import { Controller, Get, Post, Put, Patch, Delete, Param, Body, HttpCode } from '@nestjs/common';
 import { MoviesService } from './movies.service';
 
 @Controller('movies')
 export class MoviesController {
   constructor(private readonly moviesService: MoviesService) {}
 
+  // Método POST para criar um novo item
+  @Post()
+  @HttpCode(201)
+  create(@Body() createMovie: any) {// Parâmetro recebido diretamente sem DTO
+    return this.moviesService.create(createMovie);
+  }
+
+  // Método GET para listar todos os itens
   @Get()
   findAll() {
     return this.moviesService.findAll();
   }
 
+  // Método GET para buscar um item específico pelo ID
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.moviesService.findOne(+id);
   }
 
-  @Post()
-  create(@Body() body: { name: string }) {
-    return this.moviesService.create(body);
-  }
-
+  // Método PUT para substituir um item específico pelo ID
   @Put(':id')
-  update(@Param('id') id: string, @Body() body: { name: string }) {
-    return this.moviesService.update(+id, body);
+  update(@Param('id') id: string, @Body() updateMovie: any) {
+    return this.moviesService.update(+id, updateMovie);
   }
 
+  // Método PATCH para atualizar parcialmente um item pelo ID
+  @Patch(':id')
+  partialUpdate(@Param('id') id: string, @Body() partialUpdateMovie: any) {
+    return this.moviesService.update(+id, partialUpdateMovie);
+  }
+
+  // Método DELETE para remover um item específico pelo ID
   @Delete(':id')
+  @HttpCode(204)
   remove(@Param('id') id: string) {
     return this.moviesService.remove(+id);
   }
